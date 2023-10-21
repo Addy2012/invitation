573dae4cba0b3d053b7ed9ae8b2cb9822a1d2bc9
Addy2012 <kumaranand287@gmail.com>
2023-10-21 23:04:06 +0530

    creating a build

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="invitation" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
       </Routes>
